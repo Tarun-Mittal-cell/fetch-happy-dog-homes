fc5c112cb4f36e492ec886945ba48dda8bad4610
Tarun Mittal <tm6622@g.rit.edu>
2023-06-19 07:04:30 -0400

    added some styles to use-search

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,9 +1,10 @@
 import { useSearch } from "./use-search";
+import './styles.css';
 
 export const Search = () => {
   const { dogs, breeds, handleSearch, selectedBreed, setSelectedBreed } =
     useSearch();
-
+  
   return (
     <div className="search-main-container">
       <select
@@ -18,26 +19,26 @@ export const Search = () => {
       </select>
 
       <button onClick={handleSearch}>Search</button>
-
+      
       <div className="dogs-container">
         {dogs.map((dog, $index) => {
           return (
             <div key={$index} className="dog-container">
-              <div className='dog-img-container'>
+              <div className="dog-img-container">
                 <img className="dog-img" src={dog.img} />
               </div>
-              <div className='dog-name-container'>
+              <div className="dog-name-container">
                 <div className="dog-name">{dog.name}</div>
               </div>
 
-              <div className='dog-breed-container'>
+              <div className="dog-breed-container">
                 <div className="dog-breed">{dog.breed}</div>
               </div>
 
-              <div className='dog-age-container'>
+              <div className="dog-age-container">
                 <div className="dog-age">{dog.age}</div>
               </div>
-              <div className='dog-zipcode-container'>
+              <div className="dog-zipcode-container">
                 <div className="dog-zipcode">{dog.zip_code}</div>
               </div>
             </div>
